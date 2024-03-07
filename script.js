cf6f4eaa70714af8790473b1c3d4d920c618b3b6
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-03-07 21:02:06 +0200

    zero sense

--- script.js
+++ script.js
@@ -13,27 +13,34 @@ const b = data.lists[1][1];//second line
 const c = data.lists[2][1];//third line
 
 
-const NewOne = a.concat(b,c);// created a new array with all arrays included
-console.log(NewOne)
+const result = [a.concat(b,c)];// created a new array with all arrays included
+console.log(result)
 
 const { first = 1 } = data.first || {}
 const { second = 1 } = data.second || {}//not sure whats happening here, especially the first in = 1 in curly brackets
-const { third = 1 } = data.third || {}
+const { third = 1 } = data.third || {}// didn't use this at all
 
-console.log(first)
+const lastA = a.slice(-1);
+const lastB = b.slice(-1);
+const lastC = c.slice(-1);
+console.log(lastA,lastB,lastC) // testing to see if it picks the last values in each array
 
-/*const extractBiggest = () => {
-	if (a[-1] > b[-1]) {
-		return a[-1]
+//console.log(first)
+const add = result // want to a variable to use when pushing the biggest last number to new array (result)
+
+const extractBiggest = () => {
+	if (lastA > lastB) { // if last number of array A is bigger than that of aaray B
+		return add.push(lastA) //then add it to the new array result
 	}
 
-	if (c[-1] < b[-1]) {
-		return b[-1]
+	if (lastC <lastB) {
+		return add.push(lastB)
 	}
-
-	return c[-1]
+  if (lastC > lastB){
+      return add.push(lastC)
+  }
 }
-const result = []
+
 // Only edit above
 
 result.push(extractBiggest())
@@ -54,4 +61,4 @@ result.push(extractBiggest())
 result.push(extractBiggest())
 result.push(extractBiggest())
 
-console.log(result)*/
+console.log(result)
